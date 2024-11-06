1422ce6642bcb17c459f18a0b2aaa352a9cf8c1a
lecofym <166673773+lecofym@users.noreply.github.com>
2024-11-06 12:04:30 -0700

    Rarefaction_curves.R

--- R/Rarefaction_curves.R
+++ R/Rarefaction_curves.R
@@ -47,7 +47,7 @@ plot.data <- rbind(MUMPA.curve, NP.curve)
                      breaks = seq(0, 100, by = 10))+
   scale_fill_manual(values = c("dodgerblue3", "coral2")) +
   scale_color_manual(values = c("dodgerblue3", "coral2")) +
-  labs(y = 'Species richness')+
+ labs(x= 'Sampling units', y = 'Species richness') +
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.text = element_text(size = 14),
